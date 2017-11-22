256ef29e13c1f57e9227a73a51831342fc18bd2f
nandate <o238049t@mail.kyutech.jp>
2017-11-22 09:04:11 +0900

    stripe subscription object

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :plan
+
+  def make_stripe_payment(stripe_customer)
+    update!(
+      payment_method: :stripe, status: :pending_initial_payment,
+      remote_id: stripe_customer.find_subscription_for(plan)
+    )
+  end
 end
