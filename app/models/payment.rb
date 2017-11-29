a23bbe3e2faf500ec9dd0af0dd7ba4d940ffc3e6
nandate <o238049t@mail.kyutech.jp>
2017-11-29 22:55:37 +0900

    refund function(1)

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,6 +5,9 @@ class Payment < ApplicationRecord
   has_many :payment_line_items
   has_many :tickets, through: :payment_line_items,
                      source_type: "Ticket", source: "buyable"
+  has_many :refunds, class_name: "Payment",
+                     foreign_key: "original_payment_id"
+  belongs_to :original_payment, class_name: "Payment"
 
   monetize :price_cents
   enum status: [:created, :succeeded]
@@ -20,5 +23,39 @@ class Payment < ApplicationRecord
       )
     end
   end
-  
+
+  def sorted_ticket_ids
+    tickets.map(&:id).sort
+  end
+
+  def generate_refund_payment(amount_cents:, admin:)
+    refund_payment = Payment.create!(
+        user_id: user_id, price_cents: -amount_cents, status: "refund_pending",
+        payment_method: payment_method, original_payment_id: id,
+        administrator: admin, reference: Payment.generate_reference)
+    payment_line_items.each do |line_item|
+      line_item.generate_refund_payment(
+          admin: admin,
+          amount_cents: amount_cents,
+          refund_payment: refund_payment)
+    end
+    refund_payment
+  end
+
+  def payment
+    self
+  end
+
+  def maximum_available_refund
+    price + refunds.map(&:price).sum
+  end
+
+  def can_refund?(price)
+    price <= maximum_available_refund
+  end
+
+  def refund?
+    price.negative?
+  end
+
 end
